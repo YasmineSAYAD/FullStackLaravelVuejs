31fe2e840e6ebda5040ad205c3785d861bb62a2b
YasmineSAYAD <yasminesayad123@gmail.com>
2021-11-27 22:41:00 +0100

    one-way data flow

--- resources/js/bookables/BookableListItem.vue
+++ resources/js/bookables/BookableListItem.vue
@@ -8,7 +8,11 @@
 <script>
 
 export default {
-    props:{"itemTitle":String,"itemContent":String,"price":Number}
+    props:{"itemTitle":String,"itemContent":String,"price":Number},
+    mounted(){
+        console.log(this.itemTitle);
+        this.itemTitle='New title';
+    }
        
     
 }
