805d21e02009ab446599a70e3d94b11dc08f594b
YasmineSAYAD <yasminesayad123@gmail.com>
2021-11-27 21:16:12 +0100

    component registration

--- resources/js/app.js
+++ resources/js/app.js
@@ -4,8 +4,10 @@ require('./bootstrap');
 import router from "./routes";
 import VueRouter from "vue-router";
 import Vue from "vue";
+import Index from "./Index";
 window.Vue = require('vue').default;
 Vue.use(VueRouter);
+/*
 Vue.component(
     'example-component',
      require('./components/ExampleComponent.vue').default);
@@ -13,8 +15,12 @@ Vue.component(
      Vue.component(
         'second-component',
          require('./components/Example2Component.vue').default);
+         */
 const app = new Vue({
     el: '#app',
     router,
+    components:{
+        "index":Index
+    }
     
 });
